037b90b7dd8ec740cb5b34492fbd3feda3fbae65
Jordan McCollam <jordanmccollam@Jordans-MacBook-Pro.local>
2021-03-11 22:05:01 -0500

    Added a 'sheet' bar in the nav

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Navbar, Nav, Form, NavDropdown, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Nav, Form, NavDropdown, FormControl, Button, Dropdown } from 'react-bootstrap';
 import { GiTakeMyMoney } from 'react-icons/gi';
 
 const logger = "Navbar";
@@ -12,21 +12,17 @@ const CustomNav = (props) => {
       <Navbar.Brand href="#home" className="heading-05">Financial Planner <GiTakeMyMoney size={30}/></Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        {/* <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form> */}
+        <Dropdown className="navbar-dropdown ml-auto">
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            *Active Sheet*
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu className="pb-0 navbar-dropdown-menu">
+            <Dropdown.Item >*Other Sheet*</Dropdown.Item>
+            <Dropdown.Item >*Other Sheet*</Dropdown.Item>
+            <Dropdown.Item className="px-0 pb-0" ><Button className="dropdown-btn" variant="outline-primary">New</Button></Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
       </Navbar.Collapse>
     </Navbar>
   )
